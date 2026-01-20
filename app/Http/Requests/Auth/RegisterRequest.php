6d6fe9bd66e4f5ed632ed43a0bf3446312f6bbf5
Umid <umid@MacBook-Air-Umid.local>
2026-01-20 14:50:08 +0500

    feat: add auth registration and login with sanctum

---
+++ app/Http/Requests/Auth/RegisterRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users,email'],
+            'password' => ['required', 'string', 'confirmed', Password::min(8)],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'Имя обязательно для заполнения',
+            'email.required' => 'Email обязателен для заполнения',
+            'email.email' => 'Неверный формат email',
+            'email.unique' => 'Пользователь с таким email уже существует',
+            'password.required' => 'Пароль обязателен для заполнения',
+            'password.confirmed' => 'Пароли не совпадают',
+            'password.min' => 'Пароль должен быть не менее 8 символов',
+        ];
+    }
+}
